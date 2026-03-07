afe5f4c7b1a97ad91b9567793ecc9ca5df775c41
backupxelfmade <iroshfamily@gmail.com>
2026-03-07 14:06:46 -0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,6 +14,7 @@ const Navbar = () => {
     { href: '#about', label: 'About', isRoute: false },
     { href: '/services', label: 'Legal Services', isRoute: true },
     { href: '/team', label: 'Team', isRoute: true },
+    { href: '/gallery', label: 'Gallery', isRoute: true },
     { href: '/careers', label: 'Careers', isRoute: true },
     { href: '/blog', label: 'Blog', isRoute: true },
     { href: '/contact', label: 'Contact', isRoute: true },
